ec4d9594c7ae5f669dae4f9d5c0c7ac06a2c7e01
gohurd <zheka.chetverikov.322@gmail.com>
2025-08-30 19:13:58 +0300

    refactor: extract filterCustomers to function

--- src/api/customer-api.ts
+++ src/api/customer-api.ts
@@ -3,53 +3,10 @@ import type {
   Customer,
   FetchCustomersParams,
 } from "../types/customer/customer-types";
+import { filterCustomers } from "../utils/customer/filters/filter-customers";
 import type { FetchCustomersResponse } from "./types";
 
 const PER_PAGE = 25;
-
-const FILTERS = {
-  ageFrom: (customer: Customer, value?: number) => {
-    if (!value) {
-      return true;
-    }
-
-    return customer.age >= value;
-  },
-  ageTo: (customer: Customer, value?: number) => {
-    if (!value) {
-      return true;
-    }
-
-    return customer.age <= value;
-  },
-  gender: (customer: Customer, value?: string) => {
-    if (!value) {
-      return true;
-    }
-
-    return customer.gender === value;
-  },
-  location: (customer: Customer, value?: string) => {
-    if (!value) {
-      return true;
-    }
-
-    const customerLocationDetails = [
-      customer.city,
-      customer.country,
-      customer.postCode,
-      customer.state,
-      customer.street,
-      customer.streetNumber,
-    ];
-
-    const lowerCasedValue = value.toLowerCase();
-
-    return customerLocationDetails.some((customerLocation) =>
-      customerLocation.toLocaleLowerCase().includes(lowerCasedValue)
-    );
-  },
-};
 
 export const fetchCustomers = async ({
   page,
@@ -59,17 +16,7 @@ export const fetchCustomers = async ({
 
   const skip = start * PER_PAGE;
 
-  const filtered = customers.filter((customer) =>
-    Object.entries(filters).every(([key, value]) => {
-      const filterFunc = FILTERS[key as keyof typeof FILTERS];
-
-      if (!filterFunc && value) {
-        throw new Error("Filter not implemented");
-      }
-
-      return filterFunc(customer, value);
-    })
-  );
+  const filtered = filterCustomers(customers, filters);
 
   return {
     data: filtered.slice(skip, skip + PER_PAGE),
