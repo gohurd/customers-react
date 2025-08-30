246be8c5d895c8f5c4dee1c5f21936ac5a74b0ef
gohurd <zheka.chetverikov.322@gmail.com>
2025-08-30 17:50:48 +0300

    feat: customer details back icon

--- src/pages/customer/customer-details-page.tsx
+++ src/pages/customer/customer-details-page.tsx
@@ -1,12 +1,14 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useCustomerDetails } from "../../store/customer/customer-details-store";
 import { Page } from "../../ui/page";
 import { CustomerDetailsCard } from "../../features/customer/customer-details-card";
 import { CustomerDetailsOrders } from "../../features/customer/customer-details-orders";
+import back from "../../../public/arrow-back.svg";
+import { IconButton } from "../../ui/icon-button";
 
 export const CustomerDetailsPage = () => {
   const params = useParams();
-
+  const navigate = useNavigate();
   const id = Number(params.id);
 
   const { customer, error, orders } = useCustomerDetails({
@@ -24,7 +26,15 @@ export const CustomerDetailsPage = () => {
   }
 
   return (
-    <Page title="Customer details">
+    <Page
+      title="Customer details"
+      leftAddon={
+        <IconButton onClick={() => navigate(-1)}>
+          <img src={back} className="w-6 h-6 object-cover" />
+        </IconButton>
+      }
+      titleClassName="justify-start gap-4"
+    >
       {customer && !error && (
         <div className="flex-1 flex flex-col min-h-0">
           <CustomerDetailsCard customer={customer} />
